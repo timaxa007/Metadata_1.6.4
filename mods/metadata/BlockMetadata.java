a5966190f9890520201cc9069789df6debe904b7
timaxa007 <timaxa007@gmail.com>
2016-03-29 05:21:18 +0300

    Update

--- mods/metadata/BlockMetadata.java
+++ mods/metadata/BlockMetadata.java
@@ -1,8 +1,6 @@
 package mods.metadata;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -11,90 +9,75 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockMetadata extends Block {
 
-public static final String[] metadata = new String[] {
-"meta_1", 
-"meta_2", 
-"meta_3", 
-"meta_4", 
-"meta_5", 
-"meta_6", 
-"meta_7", 
-"meta_8", 
-"meta_9", 
-"meta_10", 
-"meta_11", 
-"meta_12", 
-"meta_13", 
-"meta_14", 
-"meta_15", 
-"meta_16"
-};
+	public static final String[] metadata = new String[] {
+		"meta_1", 
+		"meta_2", 
+		"meta_3", 
+		"meta_4", 
+		"meta_5", 
+		"meta_6", 
+		"meta_7", 
+		"meta_8", 
+		"meta_9", 
+		"meta_10", 
+		"meta_11", 
+		"meta_12", 
+		"meta_13", 
+		"meta_14", 
+		"meta_15", 
+		"meta_16"
+	};
 
-@SideOnly(Side.CLIENT)
-private Icon[] icon_array;
+	@SideOnly(Side.CLIENT)
+	private Icon[] icon_array;
 
-public BlockMetadata(int id) {
-super(id, Material.iron);
-setCreativeTab(ModMetadata.tab_metadata);
-setHardness(0.3F);
-setResistance(1.0F);
-setTextureName(ModMetadata.MODID + ":block_metadata");
-setUnlocalizedName("block_metadata");
-}
+	public BlockMetadata(int id) {
+		super(id, Material.circuits);
+		setCreativeTab(CreativeTabs.tabBlock);
+		setHardness(0.3F);
+		setResistance(1.0F);
+		setTextureName(ModMetadata.MODID + ":block_metadata");
+		setUnlocalizedName("block_metadata");
+	}
 
-public int quantityDropped(Random random) {
-return 0;
-}
+	@Override
+	public int damageDropped(int metadata) {
+		return metadata;
+	}
 
-@Override 
-public ArrayList<ItemStack> getBlockDropped(World world, int x, int y, int z, int metadata, int fortune) {
-ArrayList<ItemStack> ret = super.getBlockDropped(world, x, y, z, metadata, fortune);
-ret.add(new ItemStack(ModMetadata.block_metadata, 1, metadata));
-return ret;
-}
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack is) {
+		world.setBlockMetadataWithNotify(x, y, z, is.getItemDamage(), 3);
+	}
 
-public int idPicked(World world, int x, int y, int z) {
-return 0;
-}
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int metadata) {
+		if (metadata >= 0 && metadata < BlockMetadata.metadata.length) {
+			return icon_array[metadata];
+		}
+		return super.getIcon(side, metadata);
+	}
 
-public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z) {
-return new ItemStack(world.getBlockId(x, y, z), 1, world.getBlockMetadata(x, y, z));
-}
+	@SideOnly(Side.CLIENT)
+	public void getSubBlocks(int id, CreativeTabs table, List list) {
+		for (int j = 0; j < metadata.length; ++j) {
+			list.add(new ItemStack(id, 1, j));
+		}
+	}
 
-@Override
-public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack is) {
-world.setBlockMetadataWithNotify(x, y, z, is.getItemDamage(), 3);
-}
-
-@SideOnly(Side.CLIENT)
-public Icon getIcon(int side, int metadata) {
-if (metadata >= 0 && metadata < BlockMetadata.metadata.length) {
-return icon_array[metadata];
-}
-return super.getIcon(side, metadata);
-}
-
-@SideOnly(Side.CLIENT)
-public void getSubBlocks(int id, CreativeTabs table, List list) {
-for (int j = 0; j < metadata.length; ++j) {
-list.add(new ItemStack(id, 1, j));
-}
-}
-
-@SideOnly(Side.CLIENT)
-public void registerIcons(IconRegister ir) {
-super.registerIcons(ir);
-icon_array = new Icon[metadata.length];
-for (int i = 0; i < metadata.length; ++i) {
-icon_array[i] = ir.registerIcon(getTextureName() + "/" + metadata[i]);
-}
-}
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister ir) {
+		super.registerIcons(ir);
+		icon_array = new Icon[metadata.length];
+		for (int i = 0; i < metadata.length; ++i) {
+			icon_array[i] = ir.registerIcon(getTextureName() + "/" + metadata[i]);
+		}
+	}
 
 }
--- mods/metadata/ItemBlockMetadata.java
+++ mods/metadata/ItemBlockMetadata.java
@@ -5,18 +5,18 @@ import net.minecraft.item.ItemStack;
 
 public class ItemBlockMetadata extends ItemBlock {
 
-public ItemBlockMetadata(int id) {
-super(id);
-setMaxDamage(0);
-setHasSubtypes(true);
-}
+	public ItemBlockMetadata(int id) {
+		super(id);
+		setMaxDamage(0);
+		setHasSubtypes(true);
+	}
 
-public String getUnlocalizedName(ItemStack is) {
-int metadata = is.getItemDamage();
-if (metadata >= 0 && metadata < BlockMetadata.metadata.length) {
-return super.getUnlocalizedName() + "." + BlockMetadata.metadata[metadata];
-}
-return super.getUnlocalizedName();
-}
+	public String getUnlocalizedName(ItemStack is) {
+		int metadata = is.getItemDamage();
+		if (metadata >= 0 && metadata < BlockMetadata.metadata.length) {
+			return super.getUnlocalizedName() + "." + BlockMetadata.metadata[metadata];
+		}
+		return super.getUnlocalizedName();
+	}
 
 }
